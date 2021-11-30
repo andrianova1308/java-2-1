76df6545851b113fb3fe8c6962138e5496c0e254
Анастасия Самсикова <anastasiasamsikova@Anastasias-MacBook-Air.local>
2021-11-30 23:09:48 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 100;
+        int mile = 20;
+        int totalmiles = ticket / mile;
+        System.out.println(totalmiles);
+    }
+}
